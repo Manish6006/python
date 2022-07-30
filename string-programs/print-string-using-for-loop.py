0d45e94f6c2dd6f39a0e00492d5a37a015612f9f
Manish6006 <manish.sharma591@gmail.com>
2022-07-30 15:51:00 +0530

    added new folder & files

---
+++ string-programs/print-string-using-for-loop.py
@@ -0,0 +1,13 @@
+aString=None
+
+aString=input('enter the string\n')
+print(type(aString))
+for aStr in aString:
+    print(aStr,type(aStr))
+
+print('!!!!!!PART 2!!!!')
+
+for aStr in 'fewgwhrheh':
+    print(aStr, type(aStr))
+
+print('done')
