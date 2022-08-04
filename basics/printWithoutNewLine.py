09d6273e5d6fe6b5130c281814a66a0169ca08d8
Manish6006 <manish.sharma591@gmail.com>
2022-08-04 20:39:09 +0530

    created new folders and files

---
+++ basics/printWithoutNewLine.py
@@ -0,0 +1,11 @@
+n=6
+print(type(n))
+while n>0:
+    aStr=input('enter the character of your first name\n')
+    n=n-1
+
+for aString in aStr:
+    print(aStr)
+    print(aStr,end=' ')
+    
+print('finished')
