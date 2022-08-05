a86833e57c288d21aea23d7e8fd455c5c7194bcb
Manish6006 <manish.sharma591@gmail.com>
2022-08-05 12:35:06 +0530

    added more folders & files

---
+++ bit-wise-operator/right-shift.py
@@ -0,0 +1,11 @@
+''' 
+this is program example for right-shift
+'''
+
+try:
+    aInt=int(input('enter the integer value\n'))
+    print(aInt>>2,type(aInt))
+except:
+    print('terminate Abnormally')
+
+print('Finished')
