9177eebf5bc238b9e42e7288c4e61fa21b5f49b6
Manish6006 <manish.sharma591@gmail.com>
2022-08-09 12:16:13 +0530

    new folders

---
+++ loop-break-continue/continue.py
@@ -0,0 +1,14 @@
+'''
+continue helps to continue the conditional block 
+'''
+while True:
+    s = input('Enter something : ')
+    if s == 'quit':
+        break
+    if len(s) < 3:
+        print('Too small')
+        continue
+    print('Input is of sufficient length')
+    # Do other kinds of processing here...
+
+print('done')
