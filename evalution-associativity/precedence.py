0436c6a3053295c80e3c3e1503d0bf3b6442d0d6
Manish6006 <manish.sharma591@gmail.com>
2022-08-09 11:46:11 +0530

    adding modify files

--- evalution-associativity/precedence.py
+++ evalution-associativity/precedence.py
@@ -7,7 +7,7 @@ try:
     aInt=int(input('enter the first integer value\n'))
     bInt=int(input('enter the second integer value\n'))
     #without parentheses, If precedence order is same
-    result= aInt + bInt - aInt
+    result= aInt + bInt + aInt
     print('the final value is ',result,'\n')
 
     #without parentheses, If precedence order is different
@@ -15,7 +15,7 @@ try:
     print('the final value is ',result,'\n')
 
     #with parentheses, If precedence order is same
-    result= aInt + (bInt - aInt)
+    result= aInt + (bInt + aInt)
     print('the final value is ',result,'\n')
 
     #with parentheses, If precedence order is different
