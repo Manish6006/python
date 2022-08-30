280e13b57f8be4ae6393b8f137bb845964701692
Manish6006 <manish.sharma591@gmail.com>
2022-08-30 12:05:22 +0530

    added dir program file

---
+++ modules/dir_function_module.py
@@ -0,0 +1,10 @@
+'''
+This program is an example of DISPLAYING LIST OF OBJECTS OR LISTS OF MODULE
+'''
+import sys #part1
+
+print(dir(sys))
+print(__doc__)
+
+#part2 without any module
+print(dir())
