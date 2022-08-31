d0b0324c0323709f9e4680ce55d21f0edea5fa44
Manish6006 <manish.sharma591@gmail.com>
2022-08-31 11:47:58 +0530

    added length program for list

---
+++ data structure/list-length.py
@@ -0,0 +1,8 @@
+'''
+This is an example for finding the length of the list
+'''
+#defining the list 
+vegList=['cabbage','patato','tamato','ginger','carrot']
+
+#displaying the length of the list
+print('the length of the list is :', len(vegList))
