e498464a6a18a4082ba65df9c30d34a0fb5ca46f
Manish6006 <manish.sharma591@gmail.com>
2022-08-31 11:39:23 +0530

    created list data structure

---
+++ data structure/list-print.py
@@ -0,0 +1,10 @@
+'''
+This is an example for LIST data structure and their operations
+'''
+#defining the list 
+fruitList=['apple','mango','kiwi','banana','carrot']
+
+#printing the content of the defined list
+for item in fruitList:
+    print(item, end=' ')
+print('\n')
