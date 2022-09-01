ad8f9c9667f5f08b70fc34354ab90092c4a093e9
Manish6006 <manish.sharma591@gmail.com>
2022-09-01 11:48:40 +0530

    added tuple

---
+++ data structure/tuple/create-nested-tuple.py
@@ -0,0 +1,12 @@
+'''
+This is an example for CREATING NESTED TUPLE
+'''
+
+innerTuple=('first','second','third','fourth',5,6,7,8,9)
+outerTuple=('two','one','three',innerTuple)
+
+#displaying innerTuple content
+print(innerTuple)
+
+#displaying outerTuple content
+print(outerTuple)
