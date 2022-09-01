30b0869e6ff2a222f8f1269b17aed48e9ece2d1d
Manish6006 <manish.sharma591@gmail.com>
2022-09-01 12:18:52 +0530

    added new program

---
+++ data structure/tuple/create-tuple.py
@@ -0,0 +1,14 @@
+'''
+This is an example for CREATING TUPLE
+'''
+
+#defining the empty tuple
+emptyTuple=()
+
+#defining the non-empty tuple
+nonEmptyTuple=(9,3,6,1,0)
+
+#displaying the tuple items
+for item in nonEmptyTuple:
+    print(item, end=' ')
+print('\n')
