59aaa87dd6d7d23b590cba88d476462eed6c63fe
Manish6006 <manish.sharma591@gmail.com>
2022-09-02 13:54:20 +0530

    sequence program

---
+++ data structure/sequence/create-sequence.py
@@ -0,0 +1,13 @@
+'''
+This is an example for DEFINE THE SEQUENCE data structure
+'''
+
+countyName=['India','America','Singapore','Hong Kong','Chile','Peru']
+nameString='Joseph'
+
+i=1
+for item in countyName:
+    print('Item',i, ':', item)
+    i+=1
+
+print('the nameString value is :' ,nameString)
