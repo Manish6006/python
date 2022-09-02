65416b34f43444be3e81b3ec9489203240a6c460
Manish6006 <manish.sharma591@gmail.com>
2022-09-02 09:24:57 +0530

    first program file for dictionary

---
+++ data structure/dictionary/create-dictionary.py
@@ -0,0 +1,14 @@
+'''''
+This is an example for CREATING THE DICTIONARY
+'''''
+#create the dictionary
+newDictionary={
+    'firstname':'hello',
+    'middlename':'happy',
+    'lastname':'world',
+    'age':35,
+    'emailid':'helloworld@example.com',
+    'address':'computer memory'
+}
+
+print(newDictionary)
