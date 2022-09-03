9af0d69fc34428bb96644e5a6c5448e7c2b605f3
Manish6006 <manish.sharma591@gmail.com>
2022-09-03 14:13:12 +0530

    string variable renamed

--- data structure/sequence/display-index-wise.py
+++ data structure/sequence/display-index-wise.py
@@ -2,14 +2,13 @@
 This is an example for DISPLAY INDEX WISE ITEM FROM SEQUENCE
 '''
 
-countyName=['India','America','Singapore','Hong Kong','Chile','Peru']
-nameString='Joseph'
+countryName=['India','America','Singapore','Hong Kong','Chile','Peru']
 
 # Indexing
-print('Item[3] :', countyName[3])
+print('Item[3] :', countryName[3])
 
 #print the first item from the sequence
-print('Item[0] :', countyName[0])
+print('Item[0] :', countryName[0])
 
 #print the last item from the sequence
-print('Item[last] :', countyName[len(countyName)-1]) 
+print('Item[last] :', countryName[len(countryName)-1]) 
