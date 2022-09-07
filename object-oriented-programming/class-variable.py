4e41792995671b5d5b913db78eb49126c09fd4e0
Manish6006 <manish.sharma591@gmail.com>
2022-09-07 13:12:20 +0530

    added new files

---
+++ object-oriented-programming/class-variable.py
@@ -0,0 +1,5 @@
+'''
+Class variables are shared - they can be accessed by all instances of that class.
+There is only one copy of the class variable and when any one object makes a change to a class variable,
+that change will be seen by all the other instances.
+'''
