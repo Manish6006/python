e8ac7029ea6ecce5a6024bdb81880c41a65fa14c
Manish6006 <manish.sharma591@gmail.com>
2022-09-07 09:35:36 +0530

    added method inside the class

---
+++ object-oriented-programming/define-method-class.py
@@ -0,0 +1,10 @@
+'''
+This is an example for defining the Method inside the CLass
+'''
+class defineMethod:
+    def exampleMethod(self):
+        print('Calling from exampleMethod function')
+
+
+defineMethod().exampleMethod()
+
