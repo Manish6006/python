405679f514d1676dc7914af27a29c7924ad286a3
Manish6006 <manish.sharma591@gmail.com>
2022-09-09 14:16:41 +0530

    added one more oops program

---
+++ object-oriented-programming/empty-class-example.py
@@ -0,0 +1,14 @@
+''''
+This is an example for CREATING EMPTY CLASS AND LATER WE DEFINE THE VARIBLES
+'''
+
+class emptyClass():
+    pass
+
+
+objectCreated=emptyClass()
+objectCreated.name='lee'
+objectCreated.country='chile'
+objectCreated.zipcode=8372489
+
+print('Name :', objectCreated.name, 'Country :', objectCreated.country, 'Zipcode :',objectCreated.zipcode)
