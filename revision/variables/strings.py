14f67270059a8187af1fb9ecdad395866f9e40bf
MANISH KUMAR SHARMA <31389477+Manish6006@users.noreply.github.com>
2023-02-28 09:46:50 +0530

    strings variables programs

---
+++ revision/variables/strings.py
@@ -0,0 +1,30 @@
+# this is an example for Strings variables
+
+# single quotes
+str='single quotes'
+print(str)
+
+#double quotes
+str="double quotes"
+print(str)
+
+#adding single quotes
+str='doesn\'t'
+print(str)
+
+#adding double quotes
+str="doesn\'t"
+print(str)
+
+#Display '"Yes," they said.'
+str='"Yes, " they said.'
+print(str)
+
+#Display "\"Yes,\" they said."
+str="\"Yes, \" they said."
+print(str)
+
+#If you don’t want characters prefaced by \ to be interpreted 
+# as special characters, you can use raw strings by adding an r before the first quote:
+#print('C:\Users\vdshdhjtrjtrjtjtrjtrj')
+print(r'C:\Users\egrhehtrjrkkyk')
