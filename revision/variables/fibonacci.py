d1a6fcdc75e97fdb9c036acccd8249c1e87a89f2
MANISH KUMAR SHARMA <31389477+Manish6006@users.noreply.github.com>
2023-03-04 16:56:28 +0530

    new files

--- revision/variables/fibonacci.py
+++ revision/variables/fibonacci.py
@@ -1,28 +1,7 @@
 #Fibonacci series
 
-print('Enter the integer number: \n')
-
-try:
-    endNum=int(input())
-    first=0
-    second=1
-
-    print(first) #0
-    print(second) #1
-    result=first+second #1
-    for next in range(1,endNum+1):
-        result=1
-        next=1
-        result= next+result #2
-
-
-
-        
-
-
-
-
-
-except:
-    print('Entered number is not integer\n')
-
+a,b=0,1
+while a<1000:
+        print(a, end=' ')
+        a,b=b,a+b
+print()
